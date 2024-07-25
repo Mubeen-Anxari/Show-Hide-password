f371b61841ddcc80fc10bed9a93b57ef5528e9c3
Mubeen-Anxari <mubeenansari10001@gmail.com>
2024-07-25 16:56:04 +0500

    project of hide and show password

---
+++ app/components/passwords.tsx
@@ -0,0 +1,21 @@
+"use client"
+import React, { useState } from 'react'
+
+export default function Passwords() {
+    const [password,setPassword]= useState('')
+    const [isPassword, setIsPassword] =useState(false)
+
+  return (
+    <div className=' flex flex-col justify-center gap-4 items-center'>
+
+    <h1 className=' font-bold text-2xl text-white'>Enter your password and Show / Hide your password</h1>
+        <div className=' bg-blue-900 p-4 shadow-xl rounded-xl flex gap-2'>
+            <input onChange={(e)=>{setPassword(e.target.value)}} type={isPassword ? "text" :"password"} value={password} className=' w-full p-2 rounded-xl border border-green-600' />
+            <button onClick={()=>setIsPassword(prevState=>!prevState)} className='bg-purple-900 text-white rounded-xl p-1'> 
+                {isPassword ? "Hide Password": "Show Password"}
+            </button>
+
+        </div>
+    </div>
+  )
+}

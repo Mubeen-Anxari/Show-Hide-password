78c51faa230fbb358fab8452ca6ba618643135cf
Mubeen-Anxari <mubeenansari10001@gmail.com>
2024-07-25 16:59:47 +0500

    change the input

--- app/components/passwords.tsx
+++ app/components/passwords.tsx
@@ -10,7 +10,7 @@ export default function Passwords() {
 
     <h1 className=' font-bold text-2xl text-white'>Enter your password and Show / Hide your password</h1>
         <div className=' bg-blue-900 p-4 shadow-xl rounded-xl flex gap-2'>
-            <input onChange={(e)=>{setPassword(e.target.value)}} type={isPassword ? "text" :"password"} value={password} className=' w-full p-2 rounded-xl border border-green-600' />
+            <input onChange={(e)=>{setPassword(e.target.value)}} placeholder='Enetr Your Password' type={isPassword ? "text" :"password"} value={password} className=' w-full p-2 rounded-xl border border-green-600' />
             <button onClick={()=>setIsPassword(prevState=>!prevState)} className='bg-purple-900 text-white rounded-xl p-1'> 
                 {isPassword ? "Hide Password": "Show Password"}
             </button>
